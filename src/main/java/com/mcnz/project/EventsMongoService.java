8f31a01534b5ba9cd2a5f97f8548cb3891e27bdf
Bob Smith <wasadmin@webagesolutions.com>
2023-08-03 21:34:59 -0400

    done

--- src/main/java/com/mcnz/project/EventsMongoService.java
+++ src/main/java/com/mcnz/project/EventsMongoService.java
@@ -8,36 +8,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EventsMongoService implements EventsService {
-	
+
 	@Autowired
 	EventsMongoRepository repo;
-	
+
 	@Override
 	public void loadData() {
 		Event event1 = new Event("abc", "Cars and coffee", "Fun Event");
 		Event event2 = new Event("def", "Taylor", "hahaha");
-		Event event3 = new Event("Whatever","Muse","Gogogo");
+		Event event3 = new Event("Whatever", "Muse", "Gogogo");
 		repo.save(event1);
 		repo.save(event2);
 		repo.save(event3);
 	}
-	
-	
+
 	@Override
 	public Optional<Event> findEventById(String id) {
-		// TODO Auto-generated method stub
 		return repo.findById(id);
 	}
 
 	@Override
 	public void deleteEventById(String id) {
-		// TODO Auto-generated method stub
 		repo.deleteById(id);
 	}
 
 	@Override
 	public List<Event> findAllEvents() {
-		// TODO Auto-generated method stub
 		return repo.findAll();
 	}
 
--- src/main/java/com/mcnz/project/ServiceGateway.java
+++ src/main/java/com/mcnz/project/ServiceGateway.java
@@ -14,41 +14,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceGateway {
-	
+
 	@Autowired
 	public EventsService eventService;
-	
+
 	@GetMapping("/loaddata")
 	public void loaddata() {
-		Event e1 = new Event("ABC","Concert","music concert");
+		Event e1 = new Event("ABC", "Concert", "music concert");
 		eventService.saveOrUpdate(e1);
 	}
-	
+
 	@GetMapping("/events")
 	public List<Event> findAllEvents() {
 		return eventService.findAllEvents();
 	}
-	
+
 	@GetMapping("/events/{id}")
 	public Optional<Event> findEventById(@PathVariable String id) {
 		return eventService.findEventById(id);
 	}
-	
+
 	@DeleteMapping("/events/{id}")
 	public void deleteEventById(@PathVariable String id) {
 		eventService.deleteEventById(id);
 	}
-	
+
 	@PostMapping("/events")
 	public Event create(@RequestBody Event event) {
 		event = eventService.saveOrUpdate(event);
 		return event;
 	}
-	
+
 	@PutMapping("/events/{id}")
 	public Event update(@RequestBody Event event) {
 		event = eventService.saveOrUpdate(event);
 		return event;
 	}
-	
+
 }
